d8cf76f11ce88be964e8f2b53fdf01f29bbfc322
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-11-27 19:11:05 +0000

    added components & created context

---
+++ src/store/timers-context.tsx
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+
+type Timer = {
+  name: string;
+  duration: number;
+};
+
+type TimersState = {
+  isRunning: boolean;
+  timers: Timer[];
+};
+
+type TimeContextValue = TimersState & {
+  addTimer: (timerData: Timer) => void;
+  startTimers: () => void;
+  stopTimers: () => void;
+};
+
+const TimersContext = createContext<TimeContextValue | null>(null);
